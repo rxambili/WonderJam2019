e4e5877a99a1ad7696f23f736024f220ce1cdb63
Robin Xambili <r.xambili@gmail.com>
2019-03-01 21:55:26 -0500

    Add scriptableObjects Punchline & Effect

---
+++ Assets/Scripts/Punchline.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PunchlineCategory{
+    CLASH,
+    EGO
+}
+
+[CreateAssetMenu(fileName = "Punchline", menuName = "Action/Punchline", order = 1)]
+public class Punchline : ScriptableObject
+{
+    public string title;
+    public string line;
+    public int flowCost;
+    public PunchlineCategory category;
+
+    public List<Effect> effects = new List<Effect>();
+
+    
+}
