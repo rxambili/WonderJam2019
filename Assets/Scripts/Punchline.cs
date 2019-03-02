fd3ccadb2968c405d1174b137ecf369236bf7c9b
Robin Xambili <r.xambili@gmail.com>
2019-03-01 22:25:35 -0500

    Add counter punchline

--- Assets/Scripts/Punchline.cs
+++ Assets/Scripts/Punchline.cs
@@ -10,12 +10,15 @@ public enum PunchlineCategory{
 [CreateAssetMenu(fileName = "Punchline", menuName = "Action/Punchline", order = 1)]
 public class Punchline : ScriptableObject
 {
-    public string title;
-    public string line;
-    public int flowCost;
+    public string title = "...";
+    public string line = "...";
+    public int flowCost = 0;
     public PunchlineCategory category;
 
     public List<Effect> effects = new List<Effect>();
 
+    public bool hasCounter = false;
+
+    public string counter = "...";
     
 }
