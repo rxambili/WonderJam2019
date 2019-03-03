84488ac72cb7315eeec3ac8fc4f44aeb346fa0f6
Olivier Tinh <olivier.tinh@gmail.com>
2019-03-03 10:39:48 -0500

    Added transition phase

--- Assets/Scripts/Start Menu/PlayerReadyText.cs
+++ Assets/Scripts/Start Menu/PlayerReadyText.cs
@@ -18,8 +18,9 @@ public class PlayerReadyText : MonoBehaviour
 
     public void setReady()
     {
-        playerSelection.text = "READY";
-        ready = true;
+        // playerSelection.text = "READY";
+        // ready = true;
         onReady.raise();
+        gameObject.SetActive(false);
     }
 }
---
+++ Assets/Scripts/Start Menu/PlayerTransitionAnimation.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class PlayerTransitionAnimation : MonoBehaviour
+{
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void setRunningAnimation()
+    {
+        animator.SetTrigger("RunTransition");
+    }
+}
